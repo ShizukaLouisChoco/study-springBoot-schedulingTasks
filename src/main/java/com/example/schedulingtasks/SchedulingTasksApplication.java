0d2a7983bd111514fad1f1b8ff5fbc104b21ba66
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-01 11:36:50 +0100

    1) Adding the awaitility dependency

--- src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
+++ src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchedulingTasksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchedulingTasksApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingTasksApplication.class, args);
+    }
 
 }
