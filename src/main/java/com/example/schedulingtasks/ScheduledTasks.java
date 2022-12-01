d2e33e478f80452baaa1287c4324d3a598cae6f6
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-01 13:50:31 +0100

    補足 loggerFactory, SimpleDataFormat, @Scheduled

--- src/main/java/com/example/schedulingtasks/ScheduledTasks.java
+++ src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -15,6 +15,8 @@
  */
 package com.example.schedulingtasks;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -34,3 +36,28 @@ public class ScheduledTasks {
         log.info("The time is now{}", dataFormat.format(new Date()));
     }
 }
+
+/*
+loggerFactory
+https://waman.hatenablog.com/entry/20070826/1188153571
+
+SimpleDateFormat
+https://magazine.techacademy.jp/magazine/18611
+パターン文字:意味
+  G :紀元
+  y :年
+  M :年における月
+  E :曜日の名前
+  H :一日における時(0 - 23)
+  h :午前または午後の時(1 - 12)
+  m :分
+  s :秒
+
+@Scheduled
+https://reasonable-code.com/spring-boot-scheduling-tasks/
+パラメータ
+fixedDelay：メソッドの前回の実行完了時刻から何ミリ秒後に実行するか。
+fixedRate：メソッドの前回の実行開始時刻から何ミリ秒後に実行するか。
+initialDelay：メソッドの初回実行時の待機時間。単位はミリ秒。
+cron：cronによるスケジュール設定。zoneによるタイムゾーンの指定も可能。
+*/
